f998a6443144cd7435544ea2704d8e7a0c81e0cc
berke.yz <berkeyilmaz189@gmail.com>
2024-10-30 14:08:57 +0300

    IRepositoryManager

---
+++ Repositories/EFCore/RepositoryManager.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.EFCore
+{
+    public class RepositoryManager : IRepositoryManager
+    {
+
+        private readonly RepositoryContext _context;
+
+        public RepositoryManager(RepositoryContext repositoryContext)
+        {
+            _context = repositoryContext;
+        }
+
+        public IBookRepository Book => new BookRepository(_context);
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
--- Repositories/obj/Debug/net6.0/Repositories.AssemblyInfo.cs
+++ Repositories/obj/Debug/net6.0/Repositories.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6dedd8ce0e9779f6340f984710a608d862b5cdb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b75dae33ba808f7095749088da9cd6b263469b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
