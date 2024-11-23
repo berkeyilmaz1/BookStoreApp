e4a3192eb6eb9e36513aae7553c358771a108d9d
berke.yz <berkeyilmaz189@gmail.com>
2024-11-23 14:54:33 +0300

    katmanlı mimari ve nlog

--- Entities/obj/Debug/net6.0/Entities.AssemblyInfo.cs
+++ Entities/obj/Debug/net6.0/Entities.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9acd8a5f241d9542e41891cbe6b14cfbfd31aadf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23a48df187db6c6ae83bca836f0fc749d86debdf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
